038f7fa02dca8ed33bde78500f800e615be30143
Halicrea <elyna.bouchereau@outlook.fr>
2022-11-07 00:00:39 +0100

    Implementing the other modules for trace generation (not functionnal).

--- main_TraceON.cpp
+++ main_TraceON.cpp
@@ -7,15 +7,16 @@
 #include "trace_generator.hpp"
 #include <iostream>
 #include <regex>
-
 using namespace std;
 
-
-
 int main(){
-    string parameter="E2<()+>E8()E46<()*>S";
+    string parameter="E2<()+>E8()E12<()E13|E89|E41%4|E14%8>E46<()*>S";
+	//string parameter;
+	//parameter+=argv[1];
     smatch trace;
     smatch cas;
+	string anchor_list = "";
+	string test_cas = "";
     string sequence = "";
 	string temporary_anchor = "";
 	int first_anchor_value;
@@ -28,14 +29,11 @@ int main(){
 
         // On prend la 1ère trace
         regex_search(parameter,trace,regex("(^E[0-9]*)"));
-        //trace="E1";
-		sequence += trace.str(1);
-        
-        //sequence += trace.str(1);
+		sequence += trace.str(1) + ' ';
+
 		temporary_anchor=trace.str(1);
 		first_anchor_value = atoi(temporary_anchor.substr(1,temporary_anchor.length()-1).c_str());
         // On test le cas
-        //regex_search(parameter,cas,regex("(<\()"));
 		parameter.erase(0,trace.str(1).length());
 
         if(parameter[0]=='('){
@@ -47,18 +45,29 @@ int main(){
 			if(parameter[3]=='+' || parameter[3]=='*'){
 				cout << "Cas +\n";
 				a.set_temps();
-				
+
 				a.crochets_plus_or_star(first_anchor_value,parameter[3]);
 				sequence += a.get_bloc();
 				cout << "Add and erase\n";
 				parameter.erase(0,5);
-			} else {break;}
+			} else {
+				regex_search(parameter,cas,regex("(E[0-9]*\\|)"));
+				test_cas += cas.str(1);
+				if(test_cas.back()=='|'){
+					regex_search(parameter,cas,regex("([|E0-9%]*\\|E[0-9%]*)"));
+					anchor_list += cas.str(1);
+					cout << "Cas |: " << anchor_list << endl;
+					a.set_temps();
+					//a.crochets_pipe(first_anchor_value,);
+					parameter.erase(0,(anchor_list.length()+4));
+				} else {break;}
+				
+			}
 		}
 
         //cout << "Paramètre coupé: " << parameter << endl;
     }
     sequence += "S";
     cout << sequence << endl;
-    //regex rgx("^E[0-9]*");
     return 0;
 }
--- trace_generator.cpp
+++ trace_generator.cpp
@@ -26,7 +26,7 @@ void bloc::set_temps(){
 string bloc::print_temps(){
 	string seq_tps = "";
 	for (int i=0;i<temps;i++){
-		seq_tps+='-';
+		seq_tps+="- ";
 	}
 	return seq_tps;
 }
@@ -48,10 +48,7 @@ void bloc::crochets_plus_or_star(int first_anchor_value, char etoile_croix){
 	int val_tempo = 0;
 	int val_anchor;
 	int j=0;
-	//int num_anchor1 = anchor1[1];
-	//int num_anchor2 = anchor2[1];
 	bool placing_anchor; 
-
 
 	random_device rd;
 	mt19937 gen(rd());
@@ -67,7 +64,6 @@ void bloc::crochets_plus_or_star(int first_anchor_value, char etoile_croix){
 		nbr_anchors = distr(gen);
 	}
 	int anchor_pos[nbr_anchors];
-	//auto gen_bool = bind(uniform_int_distribution<>(0,1),default_random_engine());
 
 	for (int i=0;i<nbr_anchors;i++){
 		val_tempo=uniform_int_distribution<>{0,temps-1}(gen);
@@ -93,26 +89,54 @@ void bloc::crochets_plus_or_star(int first_anchor_value, char etoile_croix){
 		if (anchor_pos[j]==i){
 			sequence += "E";
 			sequence += to_string(uniform_int_distribution<>{first_anchor_value,2*first_anchor_value}(gen));
+			sequence += ' ';
 			j++;
 		} else {
-			sequence += "-";
+			sequence += "- ";
 		}
 	}
-
-	/*
-	for (int i=0;i<temps;i++){	
-		placing_anchor=uniform_int_distribution<>{0,1}(gen);
-		if (placing_anchor){
-			sequence += 'E';
-			sequence += to_string(nbr_anchors);
-		} else {
-			sequence += '-';
-		}
-	} 
-	*/
 	this -> set_seq(sequence);
-	
 }
+
+/*void crochets_pipe(int first_anchor_value, string anchor_list){
+	// On ajoute à des positions aléatoire des anchors
+	string sequence = "";
+	int nbr_anchors = 0;
+	int val_tempo = 0;
+	int val_anchor;
+	int j=0;
+	bool placing_anchor; 
+
+	random_device rd;
+	mt19937 gen(rd());
+	if (temps == 0){
+		this -> set_seq(sequence);
+		return;
+	}
+	
+	for (int i;)
+	int anchor_pos[nbr_anchors];
+
+	for (int i=0;i<nbr_anchors;i++){
+		val_tempo=uniform_int_distribution<>{0,temps-1}(gen);
+		while (find(anchor_pos,val_tempo)){
+			val_tempo=uniform_int_distribution<>{0,temps-1}(gen);
+		}
+		anchor_pos[i]=val_tempo;
+	}
+	quickSort(anchor_pos,0,nbr_anchors-1);
+	for (int i=0; i<temps;i++){
+		if (anchor_pos[j]==i){
+			sequence += "E";
+			sequence += to_string(uniform_int_distribution<>{first_anchor_value,2*first_anchor_value}(gen));
+			sequence += ' ';
+			j++;
+		} else {
+			sequence += "- ";
+		}
+	}
+	this -> set_seq(sequence);
+}*/
 
 // Get sequence finale
 string bloc::get_bloc(){
